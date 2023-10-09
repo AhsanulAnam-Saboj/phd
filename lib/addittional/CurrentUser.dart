d8c83766214be290d20b64189b121df24aa52629
Ahsanul_Anam <ahsanulanamsaboj1999@gmail.com>
2023-10-09 23:32:11 +0600

    importing files

--- lib/addittional/CurrentUser.dart
+++ lib/addittional/CurrentUser.dart
@@ -1,17 +1,7 @@
-<<<<<<< HEAD
-// import 'package:phd/addittional/User.dart';
-//
-//
-//  usser currentUser = usser(name: 'Ahsanul Anam', imageurl: 'assets/mine.png');
-//
-//
-//
-=======
+
 import 'package:phd/addittional/User.dart';
 
 
  usser currentUser = usser(name: 'Ahsanul Anam', imageurl: 'assets/mine.png');
 
 
-
->>>>>>> origin/master
